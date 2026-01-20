c2824fe9a3b93bef7c37c37940de3a2c51da08b3
deep2555 <deepanshuprashar87@gmail.com>
2026-01-20 18:07:27 +0100

    new file added

--- OrderProcessingSystem/src/com/deepanshu/orderprocessing/filehandling/FileHandlingProcess.java
+++ OrderProcessingSystem/src/com/deepanshu/orderprocessing/filehandling/FileHandlingProcess.java
@@ -1,5 +1,28 @@
 package com.deepanshu.orderprocessing.filehandling;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+ * file handling class to deal with the reading and writing or file related process
+*/
 public class FileHandlingProcess {
 
+	
+	public void readFileFromLocation(String fileLocation) {
+		
+		try(BufferedReader fileReader = new BufferedReader(new FileReader(fileLocation))){
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
